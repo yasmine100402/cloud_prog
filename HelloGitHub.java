fd3865335b6b81eb0e2c0510bf398156be2634df
jayrold <127464544+blackhatsV1@users.noreply.github.com>
2025-02-10 15:13:53 +0800

    collab Update HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,5 +1,13 @@
+import java.time.LocalDate;  
+import java.time.LocalTime;
+
 public class HelloGitHub {
     public static void main(String[] args) {
     System.out.println("Hello, GitHub!");
+
+    LocalDate mydate = LocalDate.now();  
+    LocalTime mytime = LocalTime.now();
+    System.out.println("Date: "+ mydate);
+    System.out.println("Time: "+ mytime);
     }
     }
